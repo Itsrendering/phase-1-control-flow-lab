440e3d6b6d6e52bf8fd02bc46bbee826fc0da894
Juanpaolo Delacruz <jpmd.creates@gmail.com>
2024-04-23 20:37:46 -0400

    completed

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting;
+  if (feet<=400){
+      greeting = 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000){
+      greeting = 'That will be twenty bucks.';
+  }
+  else if (feet > 2500){
+      greeting='No can do.'
+  }
+  else if (feet > 2000){
+      greeting='I will gladly take your thirty bucks.'
+  }
+  return greeting;
+  }
+
+
+function ternaryCheckCity(city){
+    return city ==='NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+    switch (tip) {
+        case 'generous':
+            return 'Thank you so much.';
+        case 'not as generous':
+            return 'Thank you.';
+        default:
+            return 'Bye.';
+    }
 }
